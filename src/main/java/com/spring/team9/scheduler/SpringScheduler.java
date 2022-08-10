ac107268d6e5e71c53927795e0dbf69391ede45d
Moveuk <v_donguk@naver.com>
2022-08-10 15:14:57 +0900

    [#26]add: 댓글 없는 게시글 삭제 스케쥴러 기능 추가

--- src/main/java/com/spring/team9/scheduler/SpringScheduler.java
+++ src/main/java/com/spring/team9/scheduler/SpringScheduler.java
@@ -2,6 +2,9 @@ package com.spring.team9.scheduler;
 
 import com.amazonaws.services.s3.model.DeleteObjectsRequest;
 import com.spring.team9.S3.S3Service;
+import com.spring.team9.model.Comment;
+import com.spring.team9.model.Contents;
+import com.spring.team9.repository.CommentRepository;
 import com.spring.team9.repository.ContentsRepository;
 import com.spring.team9.repository.mapping.ImgUrlMapping;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final 멤버 변수를 자동으로 생성합니다.
@@ -16,11 +20,12 @@ import java.util.stream.Collectors;
 public class SpringScheduler {
 
     private final ContentsRepository contentsRepository;
+    private final CommentRepository commentRepository;
     private final S3Service s3Service;
 
     // 초, 분, 시, 일, 월, 주 순서
     @Scheduled(cron = "0 0 2 * * *") // 매일 02시 스케쥴
-    public void deleteOrphanImage() throws InterruptedException {
+    public void deleteOrphanImage() {
         List<String> dbImgList = contentsRepository.findAllBy(ImgUrlMapping.class) // url 주소 값만 받아옴.
                 .stream()   // 스트림
                 .filter(url -> url != null)   // url null 제거
@@ -39,5 +44,17 @@ public class SpringScheduler {
 
         // S3 삭제요청
         s3Service.deleteObjects(keys);
+    }
+
+    @Scheduled(cron = "0 0 1 * * *") // 매일 01시 스케쥴
+    public void deleteContentsHavaNotAnyComment() {
+        Set<Long> contentIdList = contentsRepository.findAll().stream().map(Contents::getId).collect(Collectors.toSet()); //전체글 id 리스트
+        Set<Long> contentHaveCommentList = commentRepository.findAll().stream().map(Comment::getContent).map(Contents::getId).collect(Collectors.toSet());  // 댓글 가지고 있는 id list
+
+        List<Long> deleteList = contentIdList.stream().filter(contentId -> !contentHaveCommentList.contains(contentId)).collect(Collectors.toList());   // 전체리스트에서 댓글 가지고 있는 id 값 제거
+
+        deleteList.stream().forEach(contentId -> {
+            contentsRepository.deleteById(contentId);
+        });
     }
 }
